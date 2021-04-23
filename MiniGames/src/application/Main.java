ae204e72350fe6fbd1f7052453b7b7fbe7465a0f
Paula Sirisumpund <paulaclark30@yahoo.com>
2021-04-23 14:34:16 -0500

    First commit, Added .fxml files and controllers

---
+++ MiniGames/src/application/Main.java
@@ -0,0 +1,29 @@
+  
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			primaryStage.setTitle("TicTacSudoku"); //Sets the title of the application
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Main.fxml")); //Obtains main menu screen
+			Scene scene = new Scene(root,750,500); //Size of the screen
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm()); 
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}//end method start
+	
+	public static void main(String[] args) {
+		launch(args); //Launches app
+	} //end main method
+} //end class main
