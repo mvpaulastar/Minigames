98aa4d0af24335a1057159792fae952994a89d7e
Star579 <stellanova579@gmail.com>
2021-04-23 23:06:57 -0500

    Added stage to TicTacfxml and home button and linked menu to other pages

--- MiniGames/src/application/TicTacController.java
+++ MiniGames/src/application/TicTacController.java
@@ -1,5 +1,32 @@
 package application;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 public class TicTacController {
 
+    @FXML
+    private Button homeButton;
+    
+    
+    
+    
+    
+    //switch to Home menu
+    public void switchHome(ActionEvent event) throws IOException {
+		AnchorPane mainView = (AnchorPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+		Scene mainScene = new Scene(mainView);
+		mainScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setScene(mainScene);
+		window.show();
+	}
 }
