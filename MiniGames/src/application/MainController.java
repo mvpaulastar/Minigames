1bad4736f5147896149b1d4ae9f772fc1753cbaa
Star579 <stellanova579@gmail.com>
2021-04-23 22:35:29 -0500

    Sudoku fxml, MainContoller and Main updates

--- MiniGames/src/application/MainController.java
+++ MiniGames/src/application/MainController.java
@@ -1,17 +1,35 @@
 package application;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class MainController {
 
     @FXML
-    private Button sudokuButton;
+    private Button sudokuButton; //Button to go to the sudoku page
 
     @FXML
-    private Button leaderboardButton;
+    private Button leaderboardButton; //Button to go to the leaderboard page
 
     @FXML
-    private Button tictactoeButton;
-
+    private Button tictactoeButton; //Button to go to the tic tac toe page
+    
+    //Change scene to sudoku
+  	public void switchSudoku(ActionEvent event) throws IOException {
+  		AnchorPane sudokuView = (AnchorPane)FXMLLoader.load(getClass().getResource("Sudoku.fxml"));
+		Scene sudokuScene = new Scene(sudokuView);
+		sudokuScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setScene(sudokuScene);
+		window.show();
+  	}
+    
 }
